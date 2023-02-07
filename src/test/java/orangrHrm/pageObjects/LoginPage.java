e1c3c2eee71fb61e89c4c77044b49e499a0b6a0e
kumaraswamy470 <kumarvangate@gmail.com>
2023-02-07 19:31:17 +0530

    removed static wait

--- src/test/java/orangrHrm/pageObjects/LoginPage.java
+++ src/test/java/orangrHrm/pageObjects/LoginPage.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.support.PageFactory;
 public class LoginPage {
 
 	WebDriver ldriver;
-	
-	 public  LoginPage(WebDriver rdriver)
+
+	public  LoginPage(WebDriver rdriver)
 	{
 		ldriver=rdriver;
 		PageFactory.initElements(rdriver, this);
@@ -18,36 +18,36 @@ public class LoginPage {
 	@FindBy(xpath="//input[@name='username']")
 	@CacheLookup
 	WebElement txtUserName;
-	
+
 	@FindBy(xpath="//input[@name='password']")
 	@CacheLookup
 	WebElement txtPassword;
-	
+
 	@FindBy(xpath="//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")
 	@CacheLookup
 	WebElement btnLogin;
-	
+
 	public void setUserName(String uname) {
-		
+
 		txtUserName.sendKeys(uname);
 	}	
-		
-public void setpassword(String pwd) 
-{
-		
-	txtPassword.sendKeys(pwd);
+
+	public void setpassword(String pwd) 
+	{
+
+		txtPassword.sendKeys(pwd);
 	}		
-		
+
 	public void submit() 
 	{
 		btnLogin.click();
 	}
-		
-		
-		
-		
-		
-		
-		
-	
+
+
+
+
+
+
+
+
 }
